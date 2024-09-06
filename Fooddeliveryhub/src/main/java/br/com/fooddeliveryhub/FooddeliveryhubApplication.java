8597d3957ec4efd25fc58c70900dd8c1cdd23a47
João Paulo Oliveira <joaooliveirawebdeveloper@gmail.com>
2024-09-05 21:01:14 -0300

    feat: configuracoes iniciais do projeto

---
+++ Fooddeliveryhub/src/main/java/br/com/fooddeliveryhub/FooddeliveryhubApplication.java
@@ -0,0 +1,13 @@
+package br.com.fooddeliveryhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FooddeliveryhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FooddeliveryhubApplication.class, args);
+	}
+
+}
---
+++ Fooddeliveryhub/src/test/java/br/com/fooddeliveryhub/FooddeliveryhubApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fooddeliveryhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FooddeliveryhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
